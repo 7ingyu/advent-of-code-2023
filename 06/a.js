c6a6611b33c8753b729adc4bc259d38a5a9b41c1
Ting-Yu Chen <7ingyu@gmail.com>
2023-12-06 15:33:30 -0800

    completed day 6 part a

---
+++ 06/a.js
@@ -0,0 +1,28 @@
+const input = require('./input')
+const [ time, distance ] = input
+  .split('\n')
+  .filter(ln => ln.length)
+  .map(ln => ln
+    .split(/\s+/)
+    .map(str => isNaN(Number(str)) ? str : Number(str))
+  )
+
+let product = 1
+
+for (let i = 1; i < time.length; i++) {
+  const ms = time[i]
+  const record = distance[i]
+  let count = 0
+
+  for (let hold = 1; hold < ms; hold ++) {
+    const traveled = hold * (ms - hold)
+    if (traveled > record) {
+      count ++
+    } else if (count > 0) {
+      break
+    }
+  }
+  product *= count
+}
+
+console.log(product)
